2dd610edce44ae3aaeb2a61c1b481147e789bad3
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-08-06 22:27:06 +0200

    f

---
+++ app/src/main/java/de/selfmade4u/localfirstpeertopeer/MainActivity.kt
@@ -0,0 +1,97 @@
+package de.selfmade4u.localfirstpeertopeer
+
+import android.content.res.Configuration
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import de.selfmade4u.localfirstpeertopeer.ui.theme.LocalFirstPeerToPeerTheme
+
+// https://developer.android.com/jetpack/compose/tutorial
+// https://developer.android.com/training/data-storage/room
+// https://developer.android.com/guide/navigation
+// https://developer.android.com/topic/architecture/data-layer/offline-first
+// https://developer.android.com/guide/topics/connectivity/wifi-aware
+// https://developer.android.com/guide/topics/connectivity/wifip2p
+// https://developer.android.com/training/connect-devices-wirelessly/nsd-wifi-direct
+// https://developers.google.com/nearby/connections/overview
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LocalFirstPeerToPeerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                ) {
+                    MessageCard(Message("Moritz", "Hi"))
+                }
+            }
+        }
+    }
+}
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(msg: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.ic_launcher_foreground),
+            contentDescription = null,
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleSmall
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun PreviewMessageCard() {
+    LocalFirstPeerToPeerTheme {
+        MessageCard(
+            msg = Message("Lexi", "Take a look at Jetpack Compose, it's great!")
+        )
+    }
+}
